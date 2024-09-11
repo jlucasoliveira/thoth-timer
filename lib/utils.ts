075ca18adea722dd71fcd83c2d6a8e86ee5122b1
jlucasoliveira <lucasosilva68@gmail.com>
2024-09-11 10:42:48 -0300

    feat(utils): add extract changes helper

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function extractChangedValues<T extends Record<string, any | any[]>>(
+  original: T,
+  updatedObject: Partial<T>,
+  ignoreArrays = true,
+): Partial<T> {
+  const result: Partial<T> = {};
+  Object.entries(updatedObject).forEach(([key, value]) => {
+    if (Array.isArray(value) && !ignoreArrays) {
+      result[key as keyof T] = value as any;
+    } else if (
+      typeof value !== "object" &&
+      key in original &&
+      value !== original[key as keyof T]
+    ) {
+      result[key as keyof T] = value;
+    }
+  });
+  return result;
+}
