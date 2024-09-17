97ffaa1fe8de5fba50ff4c5cb39cff52201d983f
jlucasoliveira <lucasosilva68@gmail.com>
2024-09-17 13:51:16 -0300

    feat: add notification handler

--- providers/app-provider.tsx
+++ providers/app-provider.tsx
@@ -4,6 +4,7 @@ import { PropsWithChildren, useState } from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
+import { Notifications } from "./notifications";
 
 export function Provider({ children }: PropsWithChildren<object>) {
   const [client] = useState(new QueryClient());
@@ -12,6 +13,7 @@ export function Provider({ children }: PropsWithChildren<object>) {
     <QueryClientProvider client={client}>
       <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
       <ReactQueryDevtools initialIsOpen={false} />
+      <Notifications />
     </QueryClientProvider>
   );
 }
---
+++ providers/notifications.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import { toast } from "@/hooks/use-toast";
+
+const storageKey = "@timer:digest-request-id";
+
+export function Notifications() {
+  const searchParams = useSearchParams();
+  const type = searchParams.get("type");
+  const digest = searchParams.get("digest");
+  const message = searchParams.get("message");
+
+  useEffect(() => {
+    const currentDigest = localStorage.getItem(storageKey);
+    if (type && message && digest && digest !== currentDigest) {
+      localStorage.setItem(storageKey, digest);
+      toast({
+        title: message,
+        variant: type === "error" ? "destructive" : "default",
+      });
+    }
+    return () => {
+      localStorage.removeItem(storageKey);
+    }
+  }, [type, message, digest]);
+
+  return null;
+}
